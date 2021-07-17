ac6663cbd2c3674a9286999aa37c21965b9e77d2
Полина Полянская <ppolina353@yandex.ru>
2021-07-17 16:59:36 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Stepik_Messenger
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
