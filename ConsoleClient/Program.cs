54597b5e47647b15def465ed8959e11440fb7844
Полина Полянская <ppolina353@yandex.ru>
2021-07-17 17:31:34 +0300

    начало проекта

--- ConsoleClient/Program.cs
+++ ConsoleClient/Program.cs
@@ -6,7 +6,7 @@ namespace Stepik_Messenger
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Start project!");
         }
     }
 }
