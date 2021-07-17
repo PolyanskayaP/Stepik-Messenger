0db725713af0ccef00bdc71f77647b095364503a
Полина Полянская <ppolina353@yandex.ru>
2021-07-17 20:50:25 +0300

    добавили класс message cs

---
+++ ConsoleClient/Message.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Stepik_Messenger
+{
+    class Message
+    {
+    }
+}
