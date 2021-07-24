dfebffa67df878f227ea45c6c90a4134915f137d
Полина Полянская <ppolina353@yandex.ru>
2021-07-24 13:40:04 +0300

    Добавлен таймер, Отпр теперь синяя

--- WindowsFormsClient/Form1.cs
+++ WindowsFormsClient/Form1.cs
@@ -21,5 +21,20 @@ namespace WindowsFormsClient
         {
 
         }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void SendButton_Click(object sender, EventArgs e)
+        {
+            UserNameTB.Text = MessageTB.Text;
+        }
     }
 }
